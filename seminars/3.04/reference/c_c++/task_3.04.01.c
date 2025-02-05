88110f4424713ea8356ad7ec263c49df3b23de13
Vladimir Zunin <vzunin@hse.ru>
2025-02-05 10:44:25 +0300

    Update sem 3.04

--- seminars/3.04/reference/c_c++/task_3.04.01.c
+++ seminars/3.04/reference/c_c++/task_3.04.01.c
@@ -2,121 +2,124 @@
 #include <stdlib.h>
 
 struct node {
-    struct node *next;
-    int info;
+  struct node *next;
+  int info;
 };
 
 // Функция освобождения памяти
 void freeMemory(struct node *lst) {
-    struct node *now = lst, *next = lst;
-    while (next) {
-        next = now->next;
-        free(now);
-        now = next;
-    }
-    puts("\nNow memory is free!");
+  struct node *now = lst, *next = lst;
+  while (next) {
+    next = now->next;
+    free(now);
+    now = next;
+  }
+  puts("\nNow memory is free!");
 }
 
 // Функция вывода списка на экран
 void printList(struct node *lst) {
-    if (!lst) {
-        puts("List is empty!");
-    } else {
-        while (lst) {
-            printf("%7d", lst->info);
-            lst = lst->next;
-        }
-        printf("\n");
+  if (!lst)
+    puts("List is empty!");
+  else {
+    while (lst) {
+      printf("%7d", lst->info);
+      lst = lst->next;
     }
+    printf("\n");
+  }
 }
 
 // Функция создания упорядоченного списка (по возрастанию)
 // Возвращает указатель на первую запись
 struct node *queueSorted() {
-    int n, prevN;
-    struct node *add, *last, *first = NULL;
-    // Сначала список пуст
+  int n, prevN;
+  struct node *add, *last, *first = NULL;
+  // Сначала список пуст
 
-    printf("Enter positive integers:\n");
-    if (!scanf("%d", &n) || n <= 0)
-        return first;
-    else{
-        first = (struct node *)malloc(sizeof(struct node));
-        // Выделим память для первой записи
-        first->info = n;
-        last = first;
-        prevN = n; // Запомним предыдущее число
+  printf("Enter positive integers:\n");
+  if (!scanf("%d", &n) || n <= 0)
+    return first;
+  else {
+    first = (struct node *)malloc(sizeof(struct node));
+    // Выделим память для первой записи
+    first->info = n;
+    last = first;
+    prevN = n; // Запомним предыдущее число
 
-        while (scanf("%d", &n) == 1 && n > 0) { // Ввод до Ctrl+Z или n<=0
+    while (scanf("%d", &n) == 1 && n > 0) {
+      // Ввод до Ctrl+Z или n<=0
 
-            if (prevN <= n) {
-                add = (struct node *)malloc(sizeof(struct node));
-                // Память для добавляемой записи
-                add->info = n; last->next = add; // Присоединим её к cписку
-                last = add;
-                prevN = n;
-            } else {
-                puts("Error: Enter new number!");
-                while (getchar() != '\n');
-            }
-        }
-        // Cделаем эту запись последней
-        last->next = NULL;
-        return first; // Вернем адрес начала списка
+      if (prevN <= n) {
+        add = (struct node *)malloc(sizeof(struct node));
+        // Память для добавляемой записи
+        add->info = n;
+        last->next = add; // Присоединим её к cписку
+        last = add;
+        prevN = n;
+      } else {
+        puts("Error: Enter new number!");
+        while (getchar() != '\n');
+      }
     }
+    // Cделаем эту запись последней
+    last->next = NULL;
+    return first; // Вернем адрес начала списка
+  }
 }
 
 // Функция вставки элемента в упорядоченный список
 struct node *insertSorted(struct node *first, int n) {
-    struct node *curr, *prev, *add;
+  struct node *curr, *prev, *add;
 
-    add = (struct node *)malloc(sizeof(struct node));
-    add->info = n;
+  add = (struct node *)malloc(sizeof(struct node));
+  add->info = n;
 
-    // Вставка в начало списка
-    if (!first)
-        add->next = NULL; first = add;
-    else {
-        if (first->info == n) {
-            add->next = first;  first = add;
-        }
-        else {
-            curr = first;
-            while (curr->next != NULL && curr->info < n) {
-                prev = curr;
-                curr = curr->next;
-            }
-            if (curr->info >= n) {
-                add->next = curr; prev->next = add;
-            }
-            else {
-                curr->next = add; add->next = NULL;
-            }
-        }
+  // Вставка в начало списка
+  if (!first) {
+    add->next = NULL;
+    first = add;
+  } else {
+    if (first->info == n) {
+      add->next = first;
+      first = add;
+    } else {
+      curr = first;
+      while (curr->next != NULL && curr->info < n) {
+        prev = curr;
+        curr = curr->next;
+      }
+      if (curr->info >= n) {
+        add->next = curr;
+        prev->next = add;
+      } else {
+        curr->next = add;
+        add->next = NULL;
+      }
     }
-
-    return first;
+  }
+  return first;
 }
 
 int main() {
-    puts("*************************************");
-    puts("*      Seminar 3.04. Task №01       *");
-    puts("*************************************\n");
+  puts("*************************************");
+  puts("*      Seminar 3.04. Task №01       *");
+  puts("*************************************\n");
 
-    struct node *first;
-    int n;
+  struct node *first;
+  int n;
 
-    first = queueSorted();
+  first = queueSorted();
+  printList(first);
+
+  printf("Enter number: ");
+  while (getchar() != '\n'); // Очистка буфера ввода
+  if (scanf("%d", &n)) {
+    first = insertSorted(first, n);
+    printf("Changed list:\n");
     printList(first);
+  }
 
-    printf("Enter number: ");
-    while (getchar() != '\n'); // Очистка буфера ввода
-    if (scanf("%d", &n)) {
-        first = insertSorted(first, n);
-        printf("Changed list:\n");
-        printList(first);
-    }
-
-    freeMemory(first);
-    return 0;
+  freeMemory(first);
+  return 0;
 }
--- seminars/3.04/reference/c_c++/task_3.04.04.cpp
+++ seminars/3.04/reference/c_c++/task_3.04.04.cpp
@@ -5,70 +5,66 @@ using namespace std;
 bool change(int a[][20], int n, int m);
 
 int main() {
-    cout << "*************************************\n";
-    cout << "*      Seminar 3.04. Task №04       *\n";
-    cout << "*************************************\n\n";
-    int a[10][20], n, m, i, *j;
-    bool k;
+  cout << "*************************************\n";
+  cout << "*      Seminar 3.04. Task №04       *\n";
+  cout << "*************************************\n\n";
+  int a[10][20], n, m, i, *j;
+  bool k;
 
-    cout << "Введите n и m" << endl;
-    cin >> n >> m;
+  cout << "Введите n и m" << endl;
+  cin >> n >> m;
 
-    cout << "Введите матрицу" << endl;
-    for (i = 0; i < n; i++)
-        for (j = a[i]; j < a[i] + m; j++)
-            cin >> *j;
+  cout << "Введите матрицу" << endl;
+  for (i = 0; i < n; i++)
+    for (j = a[i]; j < a[i] + m; j++)
+      cin >> *j;
 
-    k = change(a, n, m);
+  k = change(a, n, m);
 
-    if (!k)
-        cout << "Матрица не изменилась" << endl;
-    else {
-        cout << "Матрица после обмена" << endl;
-        for (i = 0; i < n; i++) {
-            for (j = a[i]; j < a[i] + m; j++)
-                cout << *j << "   ";
-            cout << endl;
-        }
+  if (!k)
+    cout << "Матрица не изменилась" << endl;
+  else {
+    cout << "Матрица после обмена" << endl;
+    for (i = 0; i < n; i++) {
+      for (j = a[i]; j < a[i] + m; j++)
+        cout << *j << "   ";
+      cout << endl;
     }
-
-    return 0;
+  }
+  return 0;
 }
 
 bool change(int a[][20], int n, int m) {
-    int i, j1, jp, *uj, p, *u1, *up;
-    bool k = false;
-    u1 = up = NULL;
-    
-    for (i = 0; i < n; i++) {
-        for (uj = a[i]; uj < a[i] + m; uj++) {
-            if (*uj % 2 == 0) {
-                if (u1 == NULL) {
-                    u1 = uj;
-                    j1 = uj - a[i];
-                }
-                up = uj;
-                jp = uj - a[i];
-            }
-        }
-    }
+  int i, j1, jp, *uj, p, *u1, *up;
+  bool k = false;
+  u1 = up = NULL;
 
-    if (u1 == NULL) {
-        cout << "Нет четных" << endl;
-    }
-    else if (u1 == up) {
-        cout << "Один четный" << endl;
-    }
-    else if (j1 == jp) {
-        cout << "В одном столбце" << endl;
-    }
-    else {
-        k = true;
-        for (u1 = a[0] + j1, up = a[0] + jp; u1 < a[n-1] + m; u1 += 20, up += 20) {
-            p = *u1; 
-            *u1 = *up; 
-            *up = p;
+  for (i = 0; i < n; i++)
+    for (uj = a[i]; uj < a[i] + m; uj++) {
+      if (*uj % 2 == 0) {
+        if (u1 == NULL) {
+          u1 = uj;
+          j1 = uj - a[i];
         }
+        up = uj;
+        jp = uj - a[i];
+      }
     }
-    return k;
+  if (u1 == NULL)
+    cout << "Нет четных" << endl;
+  else if (u1 == up)
+    cout << "Один четный" << endl;
+  else if (j1 == jp)
+    cout << "В одном столбце" << endl;
+  else {
+    k = true;
+    for ( u1 = a[0] + j1, up = a[0] + jp;
+          u1 < a[n - 1] + m;
+          u1 += 20, up += 20) {
+      p = *u1;
+      *u1 = *up;
+      *up = p;
+    }
+  }
+  return k;
 }
